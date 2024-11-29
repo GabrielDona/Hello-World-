7bd9f7b64e39b07f05c779e2d84ff9960ec6ea0a
Gabriel Dona <gabrieldona2004@gmail.com>
2024-11-28 22:05:22 -0300

    feat: adiciona a função somaNumeros para somar dois numeros

--- hello world.py
+++ hello world.py
@@ -1,5 +1,9 @@
 def mensagem():
     print("Essa é a mensagem corrigida!")
 
+def somaNumeros(a,b):
+    return a+b
+
 print("Hello World!")
+print(f"A soma de 5 + 10 é: {somaNumeros(5, 10)}")
 mensagem()
